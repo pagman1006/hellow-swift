8d58daac9214d8b4db945f07b2600ae1c9b0f810
Andres Gasca <andresg1006@gmail.com>
2023-11-13 00:30:10 -0600

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Andres Gasca on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
